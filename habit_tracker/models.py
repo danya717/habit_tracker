fab6a5b20c5786fce7023a3b07b4be123f548dda
danya717 <daniel.shv00@gmail.com>
2026-05-28 15:19:12 +0300

    made migrations file

--- habit_tracker/models.py
+++ habit_tracker/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class ProfileConfigurationModel(models.Model):
+    name = models.CharField(max_length=20)
+    email = models.EmailField()
+    bio = models.CharField()
