b8f9f72c9c6b5a97fd7dc228573428f55dc13b2c
danya717 <daniel.shv00@gmail.com>
2026-06-05 17:07:17 +0300

    corrected models.py

--- habit_tracker/models.py
+++ habit_tracker/models.py
@@ -1,7 +1,18 @@
 from django.db import models
 
 # Create your models here.
-class ProfileConfigurationModel(models.Model):
-    name = models.CharField(max_length=20)
+class User(models.Model):
+    full_name = models.CharField(max_length=20)
+    birth_date = models.DateField()
+    phone = models.CharField(max_length=25)
     email = models.EmailField()
     bio = models.CharField()
+
+class Streaks(models.Model):
+    streak = models.PositiveSmallIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Tasks(models.Model):
+    task = models.CharField(max_length=40)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    note = models.CharField(max_length=255, default='')
